eaa3a3951b31be242a5fe81586a2d3fb9c9c4676
Jędrzej Makuch <jedrek9291@gmail.com>
2024-11-09 20:47:45 +0100

    Adding generic repository to Shelf.Data

---
+++ Shelf.Data/Repository/IRepository/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace Shelf.Data.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T GetFirstOrDefault(Expression<Func<T, bool>> filer);
+        void Add (T entity);
+        void Delete (T entity);
+        void DeleteRange (IEnumerable<T> entities);
+    }
+}
---
+++ Shelf.Data/Repository/Repository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shelf.Data.Data;
+using Shelf.Data.Repository.IRepository;
+using System.Linq.Expressions;
+
+namespace Shelf.Data.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+        internal DbSet<T> dbSet;
+
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+            this.dbSet = _context.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public void DeleteRange(IEnumerable<T> entities)
+        {
+            dbSet.RemoveRange(entities);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = dbSet;
+            return query.ToList();
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>> filer)
+        {
+            IQueryable<T> query = dbSet;
+            query = query.Where(filer);
+            return query.FirstOrDefault();
+        }
+    }
+}
