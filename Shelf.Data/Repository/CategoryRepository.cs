8cb047565cc8615efc7c297287bc913cf95c1798
Jędrzej Makuch <jedrek9291@gmail.com>
2024-11-09 20:54:20 +0100

    Adding ICategory Repository

---
+++ Shelf.Data/Repository/CategoryRepository.cs
@@ -0,0 +1,31 @@
+﻿using Shelf.Data.Data;
+using Shelf.Data.Repository.IRepository;
+using Shelf.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shelf.Data.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        public readonly ApplicationDbContext _context;
+        public CategoryRepository(ApplicationDbContext context) :base(context)
+        {
+            _context = context;
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Update(Category category)
+        {
+            _context.Update(category);
+        }
+    }
+}
---
+++ Shelf.Data/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using Shelf.Models.Models;
+
+namespace Shelf.Data.Repository.IRepository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        void Update(Category category);
+        void Save();
+    }
+}
