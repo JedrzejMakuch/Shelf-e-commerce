6181c35604baeff5ca62802658945c38484c558a
Jędrzej Makuch <jedrek9291@gmail.com>
2024-12-03 07:48:06 +0100

    Change Register method to create ApplicationUser instead for IdentityUser

--- Shelf/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Shelf/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -18,6 +18,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
+using Shelf.Models.Models;
 
 namespace Shelf.Web.Areas.Identity.Pages.Account
 {
@@ -154,11 +155,11 @@ namespace Shelf.Web.Areas.Identity.Pages.Account
             return Page();
         }
 
-        private IdentityUser CreateUser()
+        private ApplicationUser CreateUser()
         {
             try
             {
-                return Activator.CreateInstance<IdentityUser>();
+                return Activator.CreateInstance<ApplicationUser>();
             }
             catch
             {
