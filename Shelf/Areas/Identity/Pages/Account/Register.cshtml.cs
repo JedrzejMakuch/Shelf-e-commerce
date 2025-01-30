a5b9f06e7eee62b5cae148349440cafce329045d
Jędrzej Makuch <jedrek9291@gmail.com>
2025-01-30 21:18:11 +0100

    Populate company when the user is created

--- Shelf/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Shelf/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -172,6 +172,11 @@ namespace Shelf.Web.Areas.Identity.Pages.Account
                 user.City= Input.City;
                 user.PostalCode = Input.PostalCode;
 
+                if(Input.Role == SD.Role_Company)
+                {
+                    user.CompanyId = Input.CompanyId;
+                }
+
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
