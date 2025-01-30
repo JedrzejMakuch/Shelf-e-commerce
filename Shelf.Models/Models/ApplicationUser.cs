e21bae5ca062e4a0ee091278138760e5ce7d5887
Jędrzej Makuch <jedrek9291@gmail.com>
2025-01-30 21:07:16 +0100

    Update ApplicationUserModel with Foreign Key to CompanyModel and add Company dropdown on register view

--- Shelf.Models/Models/ApplicationUser.cs
+++ Shelf.Models/Models/ApplicationUser.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Shelf.Models.Models
 {
@@ -7,10 +9,13 @@ namespace Shelf.Models.Models
     {
         [Required]
         public string Name { get; set; }
-
         public string? StreetAdress { get; set; }
         public string? City { get; set; }
         public string? State { get; set; }
         public string? PostalCode { get; set; }
+        public int? CompanyId { get; set; }
+        [ForeignKey(nameof(CompanyId))]
+        [ValidateNever]
+        public Company Company { get; set; }
     }
 }
