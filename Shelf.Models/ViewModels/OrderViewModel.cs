2bbefbcdd01c0e87b04966fe8fbb32f10649a63a
Jędrzej Makuch <jedrek9291@gmail.com>
2025-02-11 21:00:08 +0100

    Add OrderController and OrderViewModel

---
+++ Shelf.Models/ViewModels/OrderViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Shelf.Models.Models;
+
+namespace Shelf.Models.ViewModels
+{
+	public class OrderViewModel
+	{
+        public OrderHeader OrderHeader { get; set; }
+        public IEnumerable<OrderDetail> OrderDetails { get; set; }
+    }
+}
---
+++ Shelf/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Shelf.Data.Repository.IRepository;
+using Shelf.Models.Models;
+
+namespace Shelf.Web.Areas.Admin.Controllers
+{
+	public class OrderController : Controller
+	{
+		private readonly IUnitOfWork _unitOfWork;
+
+		public OrderController(IUnitOfWork unitOfWork)
+		{
+			_unitOfWork = unitOfWork;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		#region API CALLS
+
+		[HttpGet]
+		public IActionResult GetAll()
+		{
+			List<OrderHeader> orderList = _unitOfWork.OrderHeaderRepository.GetAll(includeProperties: "ApplicationUser").ToList();
+			return Json(new { data = orderList });
+		}
+
+		#endregion
+	}
+}
