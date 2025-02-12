1627c2738bae3bc44f8c1860ecc6e30323d52665
Jędrzej Makuch <jedrek9291@gmail.com>
2025-02-12 19:15:47 +0100

    Add Details View

--- Shelf/Areas/Admin/Controllers/OrderController.cs
+++ Shelf/Areas/Admin/Controllers/OrderController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Shelf.Data.Repository.IRepository;
 using Shelf.Models.Models;
+using Shelf.Models.ViewModels;
 using Shelf.Utility;
 
 namespace Shelf.Web.Areas.Admin.Controllers
@@ -20,9 +21,20 @@ namespace Shelf.Web.Areas.Admin.Controllers
 			return View();
 		}
 
-		#region API CALLS
+        public IActionResult Details(int orderId)
+        {
+            OrderViewModel orderViewModel = new OrderViewModel
+            {
+                OrderHeader = _unitOfWork.OrderHeaderRepository.GetFirstOrDefault(e => e.Id == orderId, includeProperties: "ApplicationUser"),
+                OrderDetails = _unitOfWork.OrderDetailRepository.GetAll(e => e.OrderHeader.Id == orderId, includeProperties: "Product")
+            };
 
-		[HttpGet]
+            return View(orderViewModel);
+        }
+
+        #region API CALLS
+
+        [HttpGet]
 		public IActionResult GetAll(string status)
 		{
 			IEnumerable<OrderHeader> orderList = _unitOfWork.OrderHeaderRepository.GetAll(includeProperties: "ApplicationUser").ToList();
